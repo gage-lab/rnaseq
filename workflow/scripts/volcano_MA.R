b04bf95585b5baf74bde10877fe50a67f10f5837
mikecuoco <mcuoco@ucsd.edu>
2023-03-29 13:20:36 -0700

    add volcano MA

---
+++ workflow/scripts/volcano_MA.R
@@ -0,0 +1,109 @@
+#!/usr/bin/env Rscript
+# Author: Mike Cuoco
+# Created on: Dec 29, 2022
+
+con <- file(snakemake@log[[1]], "w")
+sink(file = con, type = "message")
+
+suppressPackageStartupMessages({
+    library(ggplot2)
+    library(glue)
+    library(readr)
+    library(ggrepel)
+})
+
+ggplot2::theme_set(ggplot2::theme_bw())
+
+res <- readr::read_csv(snakemake@input[[1]])
+condition1 <- snakemake@wildcards[["condition1"]]
+condition2 <- snakemake@wildcards[["condition2"]]
+FDRcutoff <- snakemake@params[["FDRcutoff"]]
+LFCcutoff <- snakemake@params[["LFCcutoff"]]
+# save.image(glue("{snakemake@wildcards[['de']]}_plot.RData")) # for debugging
+
+n_down <- nrow(res[res$padj < FDRcutoff & res$log2FoldChange > LFCcutoff, ])
+n_up <- nrow(res[res$padj < FDRcutoff & res$log2FoldChange < -LFCcutoff, ])
+
+# MA plot
+# label significant features
+res$sig <- res$padj < FDRcutoff
+res$sig[is.na(res$sig)] <- FALSE
+
+p <- ggplot() +
+    geom_point(data = res[res$sig == F, ], aes(x = log10(baseMean), y = log2FoldChange, color = sig), alpha = 0.5) +
+    geom_point(data = res[res$sig == T, ], aes(x = log10(baseMean), y = log2FoldChange, color = sig), alpha = 0.5) +
+    geom_text_repel(
+        data = res[res$sig == T, ],
+        aes(x = log10(baseMean), y = log2FoldChange, label = .data[[snakemake@params[["label"]]]]),
+        max.overlaps = snakemake@params["max_overlaps"][[1]]
+    ) +
+    scale_color_manual(
+        values = c("FALSE" = "gray", "TRUE" = "darkred"),
+        labels = c(bquote("P"[adj] ~ ">" ~ .(FDRcutoff)), bquote("P"[adj] ~ "<" ~ .(FDRcutoff)))
+    ) +
+    labs(
+        title = glue("{condition1} vs {condition2}"),
+        caption = glue("{n_down} features downregulated; {n_up} features upregulated"),
+        color = NULL,
+        x = bquote(~ Log[10] ~ "Mean"),
+        y = bquote(~ Log[2] ~ "FoldChange")
+    ) +
+    theme(plot.title = element_text(face = "bold"))
+
+print(paste0("Saving MA plot to ", snakemake@output[["MA_plot"]]))
+ggplot2::ggsave(snakemake@output[["MA_plot"]], p, width = 10, height = 10)
+
+
+# volcano plot
+res <- res[!is.na(res$padj), ] # remove NAs
+
+# group points for coloring
+# TODO: use colorblind-friendly colors
+res$color <- character(length = nrow(res))
+res$color <- "gray"
+res$color[res$log2FoldChange > LFCcutoff] <- "darkred"
+res$color[res$log2FoldChange < -LFCcutoff] <- "darkblue"
+res$color[res$log2FoldChange > LFCcutoff & res$padj < FDRcutoff] <- "red"
+res$color[res$log2FoldChange < -LFCcutoff & res$padj < FDRcutoff] <- "blue"
+
+p <- ggplot(res, aes(x = log2FoldChange, y = -log10(padj))) +
+    geom_point(aes(fill = color, size = log10(baseMean)), color = "black", alpha = 0.5, pch = 21) +
+    scale_fill_manual(
+        values = c(
+            "darkred" = "darkred",
+            "darkblue" = "darkblue",
+            "red" = "red",
+            "blue" = "blue",
+            "gray" = "gray"
+        ),
+        breaks = c(
+            "red",
+            "blue",
+            "darkred",
+            "darkblue"
+        ),
+        labels = c(
+            bquote(~ Log[2] ~ "FC" ~ ">" ~ .(LFCcutoff) ~ "&" ~ "P"[adj] ~ "<" ~ .(FDRcutoff)),
+            bquote(~ Log[2] ~ "FC" ~ "<" ~ -.(LFCcutoff) ~ "&" ~ "P"[adj] ~ "<" ~ .(FDRcutoff)),
+            bquote(~ Log[2] ~ "FC" ~ ">" ~ .(LFCcutoff)),
+            bquote(~ Log[2] ~ "FC" ~ "<" ~ -.(LFCcutoff))
+        )
+    ) +
+    geom_text_repel(
+        data = res[res$padj < FDRcutoff & abs(res$log2FoldChange) > LFCcutoff, ],
+        aes(x = log2FoldChange, y = -log10(padj), label = .data[[snakemake@params[["label"]]]]),
+        max.overlaps = snakemake@params["max_overlaps"][[1]],
+        point_size = 0.1,
+    ) +
+    labs(
+        title = glue("{condition1} vs {condition2}"),
+        caption = glue("{n_down} features downregulated; {n_up} features upregulated}"),
+        size = bquote(~ Log[10] ~ "Mean"),
+        color = NULL,
+        x = bquote(~ Log[2] ~ "FoldChange"),
+        y = bquote(~ -Log[10] ~ "P"[adj]),
+    ) +
+    theme(plot.title = element_text(face = "bold"))
+
+print(paste0("Saving volcano plot to ", snakemake@output[["volcano_plot"]]))
+ggplot2::ggsave(snakemake@output[["volcano_plot"]], p, width = 10, height = 10)
