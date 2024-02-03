99705e8ce0c7e35ad05f9c4b80ec1d957a92b876
mikecuoco <mcuoco@ucsd.edu>
2024-02-02 19:04:03 -0800

    fix plot sizing and colors

--- workflow/scripts/pca_heatmap.R
+++ workflow/scripts/pca_heatmap.R
@@ -10,6 +10,8 @@ suppressPackageStartupMessages({
   library(PCAtools)
   library(DESeq2)
   library(paletteer)
+  library(glue)
+  library(circlize)
   library(ComplexHeatmap)
 })
 
@@ -23,6 +25,7 @@ terms <- form %>%
 # variance stabilizing transformation
 message("Performing regularized log transformation")
 vst <- DESeq2::rlog(dds, blind = TRUE)
+nsamples <- ncol(assay(vst))
 
 # PCA
 # TODO: add loadings plot/output
@@ -30,8 +33,10 @@ message("Running PCA")
 p <- PCAtools::pca(assay(vst), metadata = colData(dds))
 
 # make plots
-pdf(snakemake@output$pca)
-scree <- PCAtools::screeplot(p) + theme_classic()
+pdf(snakemake@output$pca, width = 10, height = 10)
+ncomp <- min(10, ncol(assay(vst)))
+comps <- getComponents(p, seq_len(ncomp))
+scree <- PCAtools::screeplot(p, components = comps) + theme_classic()
 print(scree)
 
 comps <- getComponents(p, seq_len(5))
@@ -48,19 +53,60 @@ dev.off()
 plot_df <- colData(dds) %>%
   as.data.frame() %>%
   dplyr::select(all_of(c(terms)))
-col_ha <- ComplexHeatmap::HeatmapAnnotation(df = plot_df)
-row_ha <- ComplexHeatmap::HeatmapAnnotation(df = plot_df, which = "row", show_legend = FALSE, show_annotation_name = FALSE)
+
+# # create color mappings
+map_colors <- function(column_name) {
+  # check if numeric
+  if (snakemake@config$de$datatypes[[column_name]] == "categorical") {
+    col <- paletteer::paletteer_d("ggsci::nrc_npg", n = length(unique(plot_df[[column_name]]))) %>% as.vector()
+    names(col) <- levels(factor(plot_df[[column_name]]))
+  } else if (snakemake@config$de$datatypes[[column_name]] == "numeric") {
+    # make continuous color palette
+    breaks <- c(min(plot_df[[column_name]]), max(plot_df[[column_name]]))
+    colors <- c("blue", "red")
+    col <- circlize::colorRamp2(breaks, colors)
+  } else {
+    stop(glue("Unknown datatype for {column_name}"))
+  }
+
+  return(col)
+}
+
+color_mappings <- lapply(names(plot_df), map_colors)
+names(color_mappings) <- names(plot_df)
+message(class(color_mappings))
+message(names(color_mappings))
+message(color_mappings)
+
+col_ha <- ComplexHeatmap::HeatmapAnnotation(df = plot_df, col = color_mappings)
+row_ha <- ComplexHeatmap::HeatmapAnnotation(df = plot_df, which = "row", show_legend = FALSE, show_annotation_name = FALSE, col = color_mappings)
 
 # euclidean distance
-pdf(snakemake@output$heatmap)
+pdf(snakemake@output$heatmap, width = 4 + (nsamples / 4), height = 4 + (nsamples / 4))
 dst <- t(assay(vst)) %>%
   dist(diag = TRUE, upper = TRUE) %>%
   as.matrix()
-p <- ComplexHeatmap::Heatmap(dst, bottom_annotation = col_ha, right_annotation = row_ha, name = "euclidean dst", column_title = "Euclidean Distance")
+p <- ComplexHeatmap::Heatmap(
+  dst,
+  bottom_annotation = col_ha,
+  right_annotation = row_ha,
+  name = "euclidean dst",
+  column_title = "Euclidean Distance",
+  width = nsamples * unit(5, "mm"),
+  height = nsamples * unit(5, "mm")
+)
 print(p)
 
 # pearson correlation
 corr <- cor(assay(vst))
-p <- ComplexHeatmap::Heatmap(corr, bottom_annotation = col_ha, right_annotation = row_ha, name = "pearson", column_title = "Pearson Correlation")
+p <- ComplexHeatmap::Heatmap(
+  corr,
+  bottom_annotation = col_ha,
+  right_annotation = row_ha,
+  name = "pearson",
+  column_title = "Pearson Correlation",
+  width = nsamples * unit(5, "mm"),
+  height = nsamples * unit(5, "mm")
+)
 print(p)
 dev.off()
